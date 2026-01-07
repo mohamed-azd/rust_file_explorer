c06d18711eedebfe32560fd6f930df131b5050d0
Mohamed Azdoud <mohamed.azd49@gmail.com>
2026-01-07 19:19:48 +0100

    feat(image_2_ascii): convert image to ascii resized to a given target width

--- image_2_ascii/src/lib.rs
+++ image_2_ascii/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use image::{DynamicImage, GenericImageView, Pixel, Rgb};
+
+pub fn convert(image: &DynamicImage, target_width: u32) -> String {
+    let mut result = String::new();
+    let resized_image = resize_image(image, target_width);
+    let pixels = resized_image.pixels();
+
+    for (index, pixel) in pixels.into_iter().enumerate() {
+        let rgb: Rgb<u8> = pixel.2.to_rgb();
+        let luminosity = get_pixel_luminosity(&rgb);
+        result.push(map_luminosity_to_char(luminosity));
+        if (index + 1) % resized_image.width() as usize == 0 {
+            result.push('\n');
+        }
+    }
+
+    result
+}
+
+fn map_luminosity_to_char(luminosity: usize) -> char {
+    let chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
+    let index: usize = (luminosity * (chars.len() - 1)) / 255;
+    chars[index]
+}
+
+fn resize_image(image: &DynamicImage, target_width: u32) -> DynamicImage {
+    let aspect_ratio = 0.5;
+    let target_height = (target_width as f64 * (image.height() as f64 / image.width() as f64) * aspect_ratio) as u32;
+    image.thumbnail(target_width, target_height)
+}
+
+fn get_pixel_luminosity(rgb: &Rgb<u8>) -> usize {
+    (0.2126 * rgb.0[0] as f64 +
+        0.7152 * rgb.0[1] as f64 +
+        0.0722 * rgb.0[2] as f64) as usize
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
